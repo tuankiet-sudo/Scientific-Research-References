290224721c6948aac25960e4f3473707f7dbbab5
hcmut-huan <huan@hcmut.edu.vn>
2024-06-12 08:35:25 +0700

    matrix implement

--- Data_Streaming_Compression/code/main.cpp
+++ Data_Streaming_Compression/code/main.cpp
@@ -12,7 +12,27 @@ void print(Matrix<int>* matrix) {
     }
 }
 
+void print(Matrix<float>* matrix) {
+    for (int i=0; i<matrix->num_row; i++) {
+        for (int j=0; j<matrix->num_col; j++) {
+            cout << matrix->cell[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main() {
-    
+    Matrix<int>* matrix = new Matrix<int>(3, 3);
+    matrix->cell[0][0] = 6; matrix->cell[0][1] = 9; matrix->cell[0][2] = 5;
+    matrix->cell[1][0] = 8; matrix->cell[1][1] = 3; matrix->cell[1][2] = 2;
+    matrix->cell[2][0] = 1; matrix->cell[2][1] = 4; matrix->cell[2][2] = 7;
+
+    Matrix<float>* inverse = matrix->inverse();
+    print(matrix);
+    cout << endl;
+    print(inverse);
+
+    delete matrix;
+    delete inverse;
     return 0;
 }
