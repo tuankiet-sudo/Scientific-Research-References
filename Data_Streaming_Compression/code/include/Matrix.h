2f2d94f1399aedb541816e628a684400041d35ca
hcmut-huan <huan@hcmut.edu.vn>
2024-06-12 08:35:25 +0700

    matrix implement

--- Data_Streaming_Compression/code/include/Matrix.h
+++ Data_Streaming_Compression/code/include/Matrix.h
@@ -33,39 +33,54 @@ struct Matrix {
     }
 
     // Gauss Jordan method
-    Matrix* inverse() {
-        Matrix* matrix = new Matrix(this->num_row, this->num_col);
-        Matrix* identity = new Matrix(this->num_row, this->num_col);
-
+    // Required square matrix
+    Matrix<float>* inverse() {
+        Matrix<float>* matrix = new Matrix<float>(this->num_row, this->num_col*2);
         for (int i=0; i<this->num_row; i++) {
             for (int j=0; j<this->num_col; j++) {
-                if (i==j) identity[i][j] = 1;
-                else identity[i][j] = 0;
+                if (i==j) matrix->cell[i][j+this->num_col] = 1;
+                else matrix->cell[i][j+this->num_col] = 0;
+
+                matrix->cell[i][j] = this->cell[i][j];
+            }
+        }
+
+        // swap rows
+        for (int i=this->num_row-1; i>0; i--) {
+            if(matrix->cell[i-1][0] < matrix->cell[i][0]) {
+                float* temp = matrix->cell[i];
+                matrix->cell[i] = matrix->cell[i-1];
+                matrix->cell[i-1] = temp;
             }
         }
 
         for (int i=0; i<this->num_row; i++) {
-            if (matrix[i][i]==0) {
-                throw "Mathematical Error!";
-                exit(-1);
-            }
             for (int j=0; j<this->num_col; j++) {
-                if (i!=j) {
-                    float ratio = matrix[j][i]/matrix[i][i];
-                    for (int k=0; k<2*n; k++) {
-                        matrix[j][k] = matrix[j][k] - ratio*matrix[i][k];
+                if (j!=i) {
+                    float ratio = matrix->cell[j][i]/matrix->cell[i][i];
+                    for (int k=0; k<this->num_col*2; k++) {
+                        matrix->cell[j][k] -= ratio*matrix->cell[i][k];
                     }
                 }
             }
         }
 
         for (int i=0; i<this->num_col; i++) {
-            for (int j=this->num_col; j<2*this->num_col; j++) {
-                matrix[i][j] = matrix[i][j]/matrix[i][i];
+            float temp = matrix->cell[i][i];
+            for (int j=0; j<this->num_col*2; j++) {
+                matrix->cell[i][j] = matrix->cell[i][j]/temp;
             }
         }
 
-        return matrix;
+        Matrix<float>* result = new Matrix<float>(this->num_col, this->num_row);
+        for (int i=0; i<num_row; i++) {
+            for (int j=0; j<num_col; j++) {
+                result->cell[i][j] = matrix->cell[i][j+this->num_col];
+            }
+        }
+
+        delete matrix;
+        return result;
     }
 
     
