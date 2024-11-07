d09b1d982d43da37d6a0a8986b3ea7c19416f610
hcmut-huan <huan@hcmut.edu.vn>
2024-11-07 22:05:00 +0700

    modify shell script and remove bat

---
+++ Data_Streaming_Compression/code/script/plot.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Start plotting..."
+
+exit 0
--- Data_Streaming_Compression/code/script/run.sh
+++ Data_Streaming_Compression/code/script/run.sh
@@ -1,39 +1,12 @@
 #!/bin/bash
 
-Compile() {
-    echo "Start compiling..."
-
-    WORK_DIR="$(pwd)"
-    rm -rf "$WORK_DIR"/bin/*
-
-    mkdir "$WORK_DIR/bin/obj/"
-    cd "$WORK_DIR/bin/obj/"
-    for file in $(find "$WORK_DIR/src/c++/" -type f); do
-        g++ -I "$WORK_DIR/include" -I "$WORK_DIR/lib" --std=c++11 -c "$file"
-    done
-
-    cd "$WORK_DIR"/bin/
-    g++ -I "$WORK_DIR/include" -I "$WORK_DIR/lib" --std=c++11 -c "$WORK_DIR/src/c++/compress.cpp"
-    g++ -I "$WORK_DIR/include" -I "$WORK_DIR/lib" --std=c++11 -c "$WORK_DIR/src/c++/decompress.cpp"
-
-    rm -f "$WORK_DIR"/bin/obj/compress.o "$WORK_DIR"/bin/obj/decompress.o 
-    g++ "$WORK_DIR"/bin/obj/*.o "$WORK_DIR"/bin/compress.o -o compress
-    g++ "$WORK_DIR"/bin/obj/*.o "$WORK_DIR"/bin/decompress.o -o decompress
-}
-
-Plot() {
-    echo "Start plotting..."
-}
-
-
-if [[ $1 = "compile" ]]; then
-    Compile
-elif [[ $1 = "compress" ]]; then
+if [[ $1 = "compress" ]]; then
+    echo "Start compressing..."
     bin/compress "${@:2}"
+
 elif [[ $1 = "decompress" ]]; then
+    echo "Start decompressing..."
     bin/decompress "${@:2}"
-elif [[ $1 = "plot" ]]; then
-    Plot "${@:2}"
 fi
 
 exit 0
