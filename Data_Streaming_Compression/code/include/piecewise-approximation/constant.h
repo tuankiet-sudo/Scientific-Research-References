92b303897963b635b53e8fc00559702b8043528a
hcmut-huan <huan@hcmut.edu.vn>
2024-11-13 17:36:38 +0700

    add hybrid-pmc

--- Data_Streaming_Compression/code/include/piecewise-approximation/constant.h
+++ Data_Streaming_Compression/code/include/piecewise-approximation/constant.h
@@ -15,3 +15,18 @@ class PMC {
         static void compress(TimeSeries& timeseries, std::string mode, float bound, std::string output);
         static void decompress(std::string input, std::string output, int interval);
 };
+
+
+class HybridPMC {
+    // Source paper: Improved Piecewise Constant Approximation Method for Compressing Data Streams
+    // Source path: src/piecewise-approximation/constant/hybrid-pmc
+    private:
+        static void _yield(BinObj* obj, int length, float value);
+        static void _approximate(IterIO& file, int interval, time_t basetime, int prev_point, int length, float value);
+        static void _merge(BinObj* obj, std::vector<float>& buffer, bool flag, int w_size, int m_window);
+        static void _split(BinObj* obj, std::vector<float>& window, float bound);
+
+    public:
+        static void compress(TimeSeries& timeseries, int w_size, int m_window, float bound, std::string output);
+        static void decompress(std::string input, std::string output, int interval);
+};
