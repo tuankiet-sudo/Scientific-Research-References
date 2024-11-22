806f680869303444b8b41a57988dead78da6f4e3
hcmut-huan <huan@hcmut.edu.vn>
2024-11-22 15:29:14 +0700

    update repo

--- Data_Streaming_Compression/code/lib/monitor.h
+++ Data_Streaming_Compression/code/lib/monitor.h
@@ -30,24 +30,24 @@ class Monitor {
 
             while (Monitor::flag) {
                 CSVObj obj;
-                {
-                    std::string ignore;
-                    std::string vsz; long rss;
-                    std::string utime; std::string stime;      
-                    std::ifstream ifs("/proc/self/stat", std::ios_base::in);    
-
-                    ifs >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore
-                        >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore
-                        >> utime >> stime >> ignore >> ignore >> ignore >> ignore
-                        >> ignore >> ignore >> ignore >> vsz >> rss;
                 
-                    obj.pushData(utime);
-                    obj.pushData(stime);
-                    obj.pushData(vsz);
-                    obj.pushData(std::to_string(rss*Monitor::page_size));
+                std::string ignore;
+                std::string vsz; long rss;
+                std::string utime; std::string stime;      
+                std::ifstream ifs("/proc/self/stat", std::ios_base::in);    
 
-                    ifs.close();
-                }
+                ifs >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore
+                    >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore
+                    >> utime >> stime >> ignore >> ignore >> ignore >> ignore
+                    >> ignore >> ignore >> ignore >> vsz >> rss;
+            
+                obj.pushData(utime);
+                obj.pushData(stime);
+                obj.pushData(vsz);
+                obj.pushData(std::to_string(rss*Monitor::page_size));
+
+                ifs.close();
+            
                 outputFile.writeStr(&obj);
                 //std::this_thread::sleep_for(milliseconds(10));
             }
